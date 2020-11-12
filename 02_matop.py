117d347db985f674ea37fc3dfbc59c9440bb3d7b
inyoung ha <pnp3737@gmail.com>
2020-11-12 21:51:30 +0900

    1112_opencv

--- 02_matop.py
+++ 02_matop.py
@@ -38,23 +38,27 @@ def func2():
 def func3():
     img1 = cv2.imread('cat.bmp')
 
-    img2 = img1
-    img3 = img1.copy()
+    img2 = img1 # 얕은 복사 img2 영상 img1의 픽셀 데이터를 공유한다
+    img3 = img1.copy() # 깊은 복사 copy()를 이용, 고양이 영상을 그대로 간직
 
     img1[:, :] = (0, 255, 255) # yellow(green + blue)
 
-    cv2.imshow('img1', img1)
-    cv2.imshow('img2', img2)
-    cv2.imshow('img3', img3)
+    cv2.imshow('img1', img1) # yellow
+    cv2.imshow('img2', img2) # yellow
+    cv2.imshow('img3', img3) # cat
     cv2.waitKey()
     cv2.destroyAllWindows()
 
+# 부분 행렬 추출
 def func4():
     img1 = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
-    img2 = img1[200:400, 200:400]
-    img3 = img1[200:400, 200:400].copy()
+    img2 = img1[200:400, 200:400] # 부분 영상 추출 / 얕은 복사로, 소스 이미지에 영향을 미친다
+    img3 = img1[200:400, 200:400].copy() # 부분 영상 추출 / copy()를 이용한 깊은 복사로, 소스 이미지에 영향을 미치지 않는다
 
     img2 += 20
+    # img3 += 20
+    print(img2)
+    # print(img3)
 
     cv2.imshow('img1', img1)
     cv2.imshow('img2', img2)
@@ -68,17 +72,28 @@ def func5():
 
     print('mat1:')
     print(mat1)
+    print(mat1.shape) # (3, 4)
 
     h, w = mat1.shape[:2]
+    print('h :', h) # 3
+    print('w :', w) # 4
 
     mat2 = np.zeros(mat1.shape, type(mat1))
+    print('mat2 :')
+    print(mat2)
+    # [[0 0 0 0]
+    # [0 0 0 0]
+    # [0 0 0 0]]
 
     for j in range(h):
         for i in range(w):
             mat2[j, i] = mat1[j, i] + 10
-
+    print('--after loop--')
     print('mat2:')
     print(mat2)
+    # [[10 11 12 13]
+    # [14 15 16 17]
+    # [18 19 20 21]]
 
 
 def func6():
@@ -98,6 +113,6 @@ if __name__ == '__main__':
     # func1()
     # func2()
     # func3()
-    # func4()
+    func4()
     # func5()
-    func6()
+    # func6()
